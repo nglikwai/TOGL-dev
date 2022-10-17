5d912314460ded26427c318296052f2fa499e10e
LIK WAI NG <nglikwai@ymail.com>
2022-10-18 00:15:11 +0800

    fix: root

--- src/components/RegisterForm/index.vue
+++ src/components/RegisterForm/index.vue
@@ -1,10 +1,10 @@
 <script>
-import { registerQuestions } from "@/src/data";
-import Checkbox from "@/src/components/common/CheckBox";
-import Footer from "@/src/components/common/Footer";
-import Tag from "@/src/components/common/Tag";
-import DatePicker from "@/src/components/common/DatePicker";
-import Input from "@/src/components/common/Input";
+import { registerQuestions } from "~/src/data";
+import Checkbox from "~/src/components/common/CheckBox";
+import Footer from "~/src/components/common/Footer";
+import Tag from "~/src/components/common/Tag";
+import DatePicker from "~/src/components/common/DatePicker";
+import Input from "~/src/components/common/Input";
 import { mapState, mapActions } from "vuex";
 import {
   Title,
